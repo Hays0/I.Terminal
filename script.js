38e12d1c09c0474cef55384769dfdbc5503b26f2
Hayso <haysoyt2022@gmail.com>
2026-02-23 13:00:36 +0000

    BUG

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function handleCommand(command) {
     print("Random Password Generated!: " + randomValue);
   }
 
-  else if (cmd === "Hello", "hello") {
+  else if (cmd === "Hello") {
     const hello = "Hello";
     print("You said hello, now I will say " + hello);
   }
